46742920a153660d9cbfc03426afd22142f6e989
Megnath Ramesh <megnath@scopear.com>
2019-03-18 16:14:47 -0600

    Initial commit - Unity Project

---
+++ DevCon-Proj/Assets/FrustumDrawer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrustumDrawer : MonoBehaviour {
+
+    Camera firstPersonCamera;
+
+	// Use this for initialization
+	void Awake() 
+    {
+        firstPersonCamera = gameObject.GetComponent<Camera>();
+	}
+
+    private void Update()
+    {
+        OnDrawGizmos();
+    }
+
+    // Update is called once per frame
+    private void OnDrawGizmos()
+    {
+        if(firstPersonCamera != null)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.matrix = firstPersonCamera.transform.localToWorldMatrix;
+            Gizmos.DrawFrustum(firstPersonCamera.transform.position, firstPersonCamera.fieldOfView, firstPersonCamera.farClipPlane, firstPersonCamera.nearClipPlane, firstPersonCamera.aspect);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        firstPersonCamera = null;
+    }
+}
